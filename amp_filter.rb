c5460cd9b7ee62ab28fb7c996455f3678e77ebf9
Juuso Mikkonen <juuso.mikkonen@aalto.fi>
2015-12-30 13:20:15 +0200

    Responsive layout for amp-img

--- amp_filter.rb
+++ amp_filter.rb
@@ -1,9 +1,14 @@
 module Jekyll
     module AmpFilter
-        def amp_images(input, wi = nil, he = nil)
+        def amp_images(input, responsive = true, wi = nil, he = nil)
+            if responsive
+                layout = ' layout="responsive"'
+            else
+                layout = ""
+            end
             width = wi || @context.registers[:site].config['img_width'] || 700
             height = he || @context.registers[:site].config['img_height'] || 300
-            input.gsub!('<img ', '<amp-img width='+width.to_s+' height='+height.to_s+' ')
+            input.gsub!('<img ', '<amp-img width='+width.to_s+' height='+height.to_s+layout+' ')
             input
         end
     end
