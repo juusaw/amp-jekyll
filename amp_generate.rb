9801c5101bdd903dca82238774112856cbfb1c83
Juuso Mikkonen <juuso.mikkonen@aalto.fi>
2016-02-21 13:04:16 +0200

    Added post metadata to amp_generate

--- amp_generate.rb
+++ amp_generate.rb
@@ -8,8 +8,11 @@ module Jekyll
       @name = 'index.html'
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'amp.html')
-      self.data['body'] = post.content
-      self.data['title'] = post.data['title']
+      self.data['body']          = post.content
+      self.data['title']         = post.data['title']
+      self.data['date']          = post.data['date']
+      self.data['author']        = post.data['author']
+      self.data['category']      = post.data['category']
       self.data['canonical_url'] = post.url
     end
   end
