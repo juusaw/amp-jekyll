f555634e16f26835f3a7ed3f139035320cec81bf
Dan Goldin <dangoldin@gmail.com>
2016-09-05 20:21:47 -0400

    Fix local size fetching

--- amp_filter.rb
+++ amp_filter.rb
@@ -20,7 +20,8 @@ module Jekyll
             src = image['src']
           else
             # FastImage doesn't seem to handle local paths when used with Jekyll
-            src = File.join('http://localhost:4000', image['src'])
+            # so let's just force the path
+            src = File.join(Dir.pwd, image['src'])
           end
           # Jekyll generates static assets after the build process.
           # This causes problems when trying to determine the dimensions of a locally stored image.
