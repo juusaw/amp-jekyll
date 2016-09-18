4906e6a6368fa0a71c8d7172adef8b33094cd2fc
ntwcklng <ntwcklng@gmail.com>
2016-09-18 22:39:08 +0200

    :boom: (#9) support liquid inside markdownify content

--- amp_generate.rb
+++ amp_generate.rb
@@ -8,7 +8,7 @@ module Jekyll
       @name = 'index.html'
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'amp.html')
-      self.data['body']          = post.content
+      self.data['body']          = (Liquid::Template.parse post.content).render site.site_payload
       self.data['title']         = post.data['title']
       self.data['date']          = post.data['date']
       self.data['author']        = post.data['author']
