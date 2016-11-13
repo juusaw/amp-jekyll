042d2dbbee9e3c6fc1b9182c7cbeaff688103884
tosbourn <toby.osbourn@googlemail.com>
2016-11-13 12:55:10 +0000

    Allows for skipping the generation of some pages

--- amp_generate.rb
+++ amp_generate.rb
@@ -22,6 +22,7 @@ module Jekyll
     def generate(site)
       dir = site.config['ampdir'] || 'amp'
       site.posts.docs.each do |post|
+        next if post.data['skip_amp'] == true
         index = AmpPost.new(site, site.source, File.join(dir, post.id), post)
         index.render(site.layouts, site.site_payload)
         index.write(site.dest)
